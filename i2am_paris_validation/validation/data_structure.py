bb8713ad4fb104b577228100256bfc926fa0f0c4
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2024-02-08 18:18:37 +0100

    Decapitalized column names in data_structure.py for use with Pyam.

--- i2am_paris_validation/validation/data_structure.py
+++ i2am_paris_validation/validation/data_structure.py
@@ -28,7 +28,7 @@ def check_value_format(df):
 
 def check_duplicates(df):
     # check if there are any duplicates (we consider duplicates those entries that have same Model, Scenario, Region and Variable)
-    df['duplicates_check'] = df.duplicated(['Model', 'Scenario', 'Region', 'Variable']).apply(lambda x: 'Duplicate' if x else '')
+    df['duplicates_check'] = df.duplicated(['model', 'scenario', 'region', 'variable']).apply(lambda x: 'Duplicate' if x else '')
 
     return df
 
@@ -37,20 +37,20 @@ def check_indices(df):
     models,regions,variables_units,variables,units,variables_units_combination = load_known_names()
 
     # check if model is valid
-    df['model_check'] = df.Model.dropna().apply(lambda x: 'Model ' + x + ' not found!' if x not in models else '')
+    df['model_check'] = df.model.dropna().apply(lambda x: 'Model ' + x + ' not found!' if x not in models else '')
 
     # check if region is valid
-    df['region_check'] = df.Region.dropna().apply(lambda x: 'Region ' + x + ' not found!' if x not in regions else '')
+    df['region_check'] = df.region.dropna().apply(lambda x: 'Region ' + x + ' not found!' if x not in regions else '')
 
     # check if variable is valid
-    df['variable_check'] = df.Variable.dropna().apply(lambda x: 'Variable ' + x + ' not found!' if x not in variables else '')
+    df['variable_check'] = df.variable.dropna().apply(lambda x: 'Variable ' + x + ' not found!' if x not in variables else '')
 
     # check if unit is valid
-    df['unit_check'] = df.Unit.dropna().apply(lambda x: 'Unit ' + x + ' not found!' if x not in units else '')
+    df['unit_check'] = df.unit.dropna().apply(lambda x: 'Unit ' + x + ' not found!' if x not in units else '')
 
     # check if variable unit combination is valid
     # fix check to avoid dropping rows
-    df['variable_unit_check']= df.dropna().apply(lambda x: f"Variable {x['Variable']} combined with unit {x['Unit']} not found!" if (x['Variable'] + ' ' + x['Unit']) not in variables_units_combination else '', axis=1)
+    df['variable_unit_check']= df.dropna().apply(lambda x: f"Variable {x['variable']} combined with unit {x['unit']} not found!" if (x['variable'] + ' ' + x['unit']) not in variables_units_combination else '', axis=1)
 
     return df
 
@@ -60,7 +60,7 @@ def check_basic_sums(df):
 
     # create a list with the aggregated variables
     aggr_vars = []
-    unique_vars = df.Variable.unique()
+    unique_vars = df.variable.unique()
     for var in unique_vars:
         var_levels = var.count('|')
 
@@ -94,17 +94,17 @@ def check_basic_sums(df):
     df['basic_sum_check'] = ''
     for variable in var_tree.keys():
         # get df that have the aggregated variable
-        agg_results = df[df['Variable'].isin(var_tree[f'{variable}'])].groupby(['Model', 'Scenario', 'Region']).sum()[numeric_columns]
+        agg_results = df[df['variable'].isin(var_tree[f'{variable}'])].groupby(['model', 'scenario', 'region']).sum()[numeric_columns]
 
         for row in agg_results.index:
             for year in numeric_columns:
-                values = df[(df['Model'] == row[0]) & (df['Scenario'] == row[1]) & (df['Region'] == row[2])  & (df['Variable'] == variable)][year].values
+                values = df[(df['model'] == row[0]) & (df['scenario'] == row[1]) & (df['region'] == row[2])  & (df['variable'] == variable)][year].values
                 if len(values) != 0:
                     # find percentage difference using the formula |x1 - x2|/((x1+x2)/2)
                     diff = (abs(values[0] - agg_results[year][row[0]][row[1]][row[2]]))/((values[0] + agg_results[year][row[0]][row[1]][row[2]])/2)
                     
                     # set difference margin at 2%
                     if diff > 0.02:
-                        df.loc[(df['Model'] == row[0]) & (df['Scenario'] == row[1]) & (df['Region'] == row[2])  & (df['Variable'] == variable), 'basic_sum_check'] += f'Basic sum check error on year {year}.     \n'
+                        df.loc[(df['model'] == row[0]) & (df['scenario'] == row[1]) & (df['region'] == row[2])  & (df['variable'] == variable), 'basic_sum_check'] += f'Basic sum check error on year {year}.     \n'
 
     return df
